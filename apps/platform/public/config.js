9aa02d073d80da6fc5ef9273a217c31bb58222c7
maniexcelra <manikantan.ravikumar@excelra.com>
2024-09-04 18:06:22 +0530

    Update config.js

--- apps/platform/public/config.js
+++ apps/platform/public/config.js
@@ -1,9 +1,9 @@
 // Default configuration is defined within the application bundle
 // DevOps context
 // var configUrlApi = 'http://localhost:8090/api/v4/graphql';
-var configUrlApi = 'http://35.237.118.186:8090/api/v4/graphql';
-var configGoogleTagManagerID = 'GTM-WPXWRDV';
-var configEFOURL = '/data/ontology/efo_json/diseases_efo.jsonl';
-var configDownloadsURL = undefined;
-var configGeneticsPortalUrl = undefined;
-var configOTAiApi = 'http://35.237.118.186:8081';
+var configUrlApi = 'http://34.23.158.19:8090/api/v4/graphql';
+// var configGoogleTagManagerID = 'GTM-WPXWRDV';
+// var configEFOURL = '/data/ontology/efo_json/diseases_efo.jsonl';
+// var configDownloadsURL = undefined;
+// var configGeneticsPortalUrl = undefined;
+// var configOTAiApi = 'http://35.237.118.186:8081';
